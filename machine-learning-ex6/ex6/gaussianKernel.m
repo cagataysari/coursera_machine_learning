12054aa47eaf86999ee21e9ec02e3ebeea228e16
Cagatay <acagataysari@gmail.com>
2016-01-08 14:26:34 +0200

    Completed the gaussian kernel code, 25 points

--- machine-learning-ex6/ex6/gaussianKernel.m
+++ machine-learning-ex6/ex6/gaussianKernel.m
@@ -7,7 +7,6 @@ function sim = gaussianKernel(x1, x2, sigma)
 x1 = x1(:); x2 = x2(:);
 
 % You need to return the following variables correctly.
-sim = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the similarity between x1
@@ -16,9 +15,11 @@ sim = 0;
 %
 %
 
-
-
-
+diff = x1-x2;
+diff_s = diff.^2;
+diff_s_s = sum(diff_s);
+sigma_s = (2*sigma*sigma);
+sim = exp(-(diff_s_s/sigma_s));
 
 
 % =============================================================
